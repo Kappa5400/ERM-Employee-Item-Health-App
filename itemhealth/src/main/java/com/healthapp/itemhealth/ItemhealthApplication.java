19207e068b4f172a400c765ac6084e003275eaf8
colin <colinburdett54@gmail.com>
2026-03-08 14:36:46 -0500

    Init

---
+++ itemhealth/src/main/java/com/healthapp/itemhealth/ItemhealthApplication.java
@@ -0,0 +1,13 @@
+package com.healthapp.itemhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemhealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemhealthApplication.class, args);
+	}
+
+}
---
+++ itemhealth/src/test/java/com/healthapp/itemhealth/ItemhealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthapp.itemhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemhealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
