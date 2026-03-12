d895206fa2152318f74cf3d02d0566514487e1f1
colin <colinburdett54@gmail.com>
2026-03-11 19:59:30 -0500

    Continuing with mapping.

---
+++ itemhealth/src/main/java/com/healthapp/itemhealth/mapper/EmployeeMapper.java
@@ -0,0 +1,17 @@
+package com.healthapp.itemhealth.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.healthapp.itemhealth.model.Employee;
+
+@Mapper
+public interface EmployeeMapper {
+    Employee findById(Long employeeId);
+    List <Employee> findAllEmployees();
+    List <Employee> findAllEmployeesBoss();
+    Employee findBossByEmployeeId(Long employeeId);
+    void insert(Employee employee);
+    void delete(Long employeeId);
+}
