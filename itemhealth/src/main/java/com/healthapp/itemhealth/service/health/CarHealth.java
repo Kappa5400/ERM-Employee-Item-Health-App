7237da0d0c373b6c62092232e06eecd1b80999a9
Kappa54 <colinburdett54@gmail.com>
2026-03-16 16:50:47 +0000

    still wip

--- itemhealth/src/main/java/com/healthapp/itemhealth/service/health/CarHealth.java
+++ itemhealth/src/main/java/com/healthapp/itemhealth/service/health/CarHealth.java
@@ -5,34 +5,34 @@ import org.springframework.stereotype.Component;
 import com.healthapp.itemhealth.model.Car;
 
 @Component
-public class CarHealth implements HealthCheck<Car>{
+public class CarHealth implements HealthCheck<Car> {
 
     private static final int CAR_MAX_AGE = 10;
     private static final int CAR_INSURANCE_FREQUENCY = 1;
     private static final int CAR_SERVICE_FREQUENCY = 3;
 
     @Override
-    public boolean checkUpdate(Car item){
+    public boolean checkUpdate(Car item) {
         return carRenewCheck(item) || carInsuranceCheck(item) || carServiceCheck(item);
     }
 
     @Override
-    public void performUpdate(Car item){
-        if (carInsuranceCheck(item))item.setToRenewInsurance(true);
-        if (carRenewCheck(item))item.setToReplace(true);
-        if (carServiceCheck(item))item.setToService(true);
+    public void performUpdate(Car item) {
+        if (carRenewCheck(item))     item.setToRenew(true);
+        if (carInsuranceCheck(item)) item.setToRenewInsurance(true);
+        if (carServiceCheck(item))   item.setToService(true);
     }
 
-    private boolean car_renew_check(Car item){
-        return item.getCar_Year() <= LocalDate.now().getYear() - CAR_MAX_AGE;
+    private boolean carRenewCheck(Car item) {                         
+        return item.getCarYear() <= LocalDate.now().getYear() - CAR_MAX_AGE;  
     }
 
-    private boolean car_insurance_check(Car item){
-        return item.getLast_Insurance_Renewal().isBefore(LocalDate.now()); 
+    private boolean carInsuranceCheck(Car item) {                     
+        return item.getInsuranceExpireDate().isBefore(LocalDate.now()); 
     }
 
-    private boolean car_service_check(Car item){
-        return item.getLastServiced().isBefore(LocalDate.now().minusMonths(CAR_SERVICE_FREQUENCY)); 
+    private boolean carServiceCheck(Car item) {                        
+        return item.getLastServiced().isBefore(
+                LocalDate.now().minusMonths(CAR_SERVICE_FREQUENCY));
     }
-
 }
