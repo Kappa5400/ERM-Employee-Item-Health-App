afa5978a7b198ed27db39afc78820169532ac858
colin <colinburdett54@gmail.com>
2026-03-26 19:28:41 -0500

    Starting on email setup

---
+++ itemhealth/src/main/java/com/healthapp/itemhealth/service/EmailService.java
@@ -0,0 +1,50 @@
+package com.healthapp.itemhealth.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import  org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+
+    public void sendEmail(String to, String subject, String body){
+        SimpleMailMessage mes = new SimpleMailMessage();
+        mes.setFrom(fromEmail);
+        mes.setTo(to);
+        mes.setSubject(subject);
+        mes.setText(body);
+        mailSender.send(mes);
+    }
+
+     public String getBossEmail(){
+        //to impliment
+        return "totest@gmail.com";
+    }
+
+    public String formatEmailSubject(){
+        return "Hi";
+    }
+
+
+    public String formatEmailBody(List items){
+        return "test";
+    }
+
+}
+
+
+
--- itemhealth/src/main/java/com/healthapp/itemhealth/service/HealthCheckService.java
+++ itemhealth/src/main/java/com/healthapp/itemhealth/service/HealthCheckService.java
@@ -1,5 +1,12 @@
 package com.healthapp.itemhealth.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
 import com.healthapp.itemhealth.model.Car;
 import com.healthapp.itemhealth.model.Employee;
 import com.healthapp.itemhealth.model.IDCard;
@@ -7,17 +14,13 @@ import com.healthapp.itemhealth.model.Laptop;
 import com.healthapp.itemhealth.service.health.CarHealth;
 import com.healthapp.itemhealth.service.health.IDCardHealth;
 import com.healthapp.itemhealth.service.health.LaptopHealth;
-import java.util.ArrayList;
-import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
 
 @Service
 public class HealthCheckService {
 
   private static final Logger log = LoggerFactory.getLogger(HealthCheckService.class);
 
+  private final EmailService emailService;
   private final EmployeeService employeeService;
   private final LaptopService laptopService;
   private final CarService carService;
@@ -26,7 +29,9 @@ public class HealthCheckService {
   private final CarHealth carHealth;
   private final IDCardHealth idCardHealth;
 
+
   public HealthCheckService(
+      EmailService emailService,
       EmployeeService employeeService,
       LaptopService laptopService,
       CarService carService,
@@ -34,6 +39,7 @@ public class HealthCheckService {
       LaptopHealth laptopHealth,
       CarHealth carHealth,
       IDCardHealth idCardHealth) {
+    this.emailService = emailService;
     this.employeeService = employeeService;
     this.laptopService = laptopService;
     this.carService = carService;
@@ -93,8 +99,14 @@ public class HealthCheckService {
       log.info(updateItems.size() + " items to update.");
 
       if (updateItems.size() != 0) {
+
+        log.info("Getting boss email...");
+        String emailAddress = emailService.getBossEmail();
+        String emailSubject = emailService.formatEmailSubject();
+        String emailBody = emailService.formatEmailBody(updateItems);
+
         log.info("Sending email...");
-        // email impliment here
+        emailService.sendEmail(emailAddress, emailSubject, emailBody);
         log.info("Email sent.");
       } else {
         log.info("No items to update.");
